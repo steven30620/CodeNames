a27a066fdeb6d87d9acbf8e3699e22f846df08ea
steven <steven30620@hotmail.fr>
2022-11-24 19:38:20 +0100

    mise en place socket io

--- src/components/game/ChatComponent.vue
+++ src/components/game/ChatComponent.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="chat">
     <div class="chat-message"></div>
-    <div class="chat-display">
+    <div
+      class="chat-display"
+      v-for="(message, index) in messages"
+      :key="index"
+      index
+    >
       <div class="chat-message">
         <div class="chat-message-user">
           <div class="chat-message-user">
@@ -11,26 +16,22 @@
                 src="../../assets/spy.png"
               />
             </div>
-            <div class="chat-message-user-name">Steven:</div>
+            <div class="chat-message-user-name">{{ message.user }}:</div>
           </div>
         </div>
-        coucou
+        {{ message.text }}
       </div>
-
-      <div class="chat-message">
-        <div class="chat-message-user">
-          <div class="chat-message-user">
-            <div class="chat-message-user-placeholder-img">
-              <img
-                class="chat-message-user-placeholder-img"
-                src="../../assets/spy.png"
-              />
-            </div>
-            <div class="chat-message-user-name">Steven:</div>
-          </div>
-        </div>
-        aurevoir
-      </div>
+    </div>
+    <div class="testUser">
+      <input
+        type="text"
+        name="chatBox"
+        class="chat-input"
+        id="chatBox"
+        placeholder="pseudo"
+        @keyup.enter="sendMessage"
+        v-model="currentUser"
+      />
     </div>
     <div class="chat-message-input">
       <input
@@ -39,7 +40,9 @@
         class="chat-input"
         id="chatBox"
         placeholder="Parlez à vos partenaire"
-      /><button class="chat-input-send-btn">
+        @keyup.enter="sendMessage"
+        v-model="messageInput"
+      /><button @click="sendMessage" class="chat-input-send-btn">
         <img class="chat-input-send-btn-img" src="../../assets/arrowSend.jpg" />
       </button>
     </div>
@@ -47,13 +50,38 @@
 </template>
 
 <script>
+import io from "socket.io-client";
 export default {
   name: "ChatComponent",
   components: {},
+  props: { joined: Boolean },
   data() {
-    return {};
+    return {
+      currentUser: "",
+      messageInput: "",
+      messages: [],
+    };
   },
-  methods: {},
+  methods: {
+    join: function () {
+      this.socketInstance = io("http://localhost:3000");
+
+      this.socketInstance.on("message:received", (data) => {
+        this.messages = this.messages.concat(data);
+      });
+    },
+    sendMessage: function () {
+      let message = {
+        user: this.currentUser,
+        text: this.messageInput,
+      };
+      this.messages = this.messages.concat(message);
+
+      this.socketInstance.emit("message", message);
+      this.messageInput = "";
+    },
+  },
+  beforeMount() {},
 };
 </script>
 
@@ -62,8 +90,8 @@ export default {
   display: flex;
   flex-direction: column;
   margin-top: 20px;
-  width: 100%;
   height: 400px;
+  min-width: 400px;
   background-color: red;
   justify-content: flex-end;
   &-display {
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,260 +1,74 @@
 <template>
-  <div class="game">
-    Au tour de l'espion Rouge de jouer !
-    <button @click="getRandomWord">Start</button>
-    <div class="game-grid col-10">
-      <div
-        v-for="(word, index) in selectedWords"
-        :key="index"
-        :class="'game-card card' + index"
-        @click="setColor(word, index)"
-      >
-        <div class="game-word-number">{{ index + 1 }}</div>
-        <div class="game-word">{{ word }}</div>
+  <div class="game-zone">
+    <GameComponent></GameComponent>
+    <div class="container" v-html="testo"></div>
+    <div class="chatZone">
+      <div class="chat">
+        <button
+          data-toggle="collapse"
+          href="#chat"
+          aria-expanded="false"
+          aria-controls="chat"
+          @click="chat"
+        >
+          Afficher le chat
+        </button>
+
+        <transition
+          name="chat-transition"
+          class="chat-transition"
+          enter-active-class="animated animate__backInLeft  animate__faster	 "
+          leave-active-class="animated animate__backOutRight animate__faster	 "
+        >
+          <ChatComponent
+            :joined="isConnected"
+            v-if="displayChat"
+            id="chat"
+          ></ChatComponent>
+        </transition>
       </div>
-    </div>
-    <div class="game-gessGrid">
-      <div
-        class="game-gessGrid-square"
-        v-for="(word, index) in selectedWords"
-        :key="index"
-        :class="setColorForGrid(word)"
-      >
-        <div class="game-gessGrid-number">{{ index + 1 }}</div>
-      </div>
-    </div>
-    <div class="game-chat">
-      <button
-        data-toggle="collapse"
-        href="#chat"
-        aria-expanded="false"
-        aria-controls="chat"
-        @click="displayChat = !displayChat"
-      >
-        Afficher le chat
-      </button>
-      <transition
-        name="chat-transition"
-        class="chat-transition"
-        enter-active-class="animated animate__backInLeft  animate__faster	 "
-        leave-active-class="animated animate__backOutRight animate__faster	 "
-      >
-        <ChatComponent v-if="displayChat" id="chat"></ChatComponent>
-      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import ChatComponent from "@/components/game/ChatComponent.vue";
-
+// import axios from "axios";
+import GameComponent from "../components/game/GameComponent";
+import ChatComponent from "../components/game/ChatComponent.vue";
 import "animate.css";
 
 export default {
-  name: "GameComponent",
+  name: "GameView",
   components: {
     ChatComponent,
+    GameComponent,
   },
+
   data() {
     return {
+      testo: "",
+      isConnected: false,
       displayChat: false,
-      selectedWords: [],
-      redWords: [],
-      blueWords: [],
-      whiteWords: [],
-      blackWord: [],
-      isRed: false,
-      isWhite: false,
-      isBlue: false,
-      isBlack: false,
     };
   },
   methods: {
-    getRandomWord: function () {
-      this.selectedWords = [];
-
-      let words = this.$store.state.words;
-      const shuffledArray = words.sort(() => 0.5 - Math.random());
-      let i = 0;
-      while (this.selectedWords.length < 25) {
-        this.selectedWords.push(shuffledArray[i]);
-        i++;
-      }
-      this.generateAnswerGrid();
-    },
-    generateAnswerGrid: function () {
-      //pour reset les tableau si on veut générer une nouvelle grille de réponse
-      (this.redWords = []),
-        (this.blueWords = []),
-        (this.whiteWords = []),
-        (this.blackWord = []);
-      let Number = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-        20, 21, 22, 23, 24,
-      ];
-      //je melange un tableau qui me servira à prendre des mot aléatoire en évitant les doublons
-      let randomNumber = Number.sort(() => 0.5 - Math.random());
-      let i = 0;
-      while (this.redWords.length < 9) {
-        this.redWords.push(this.selectedWords[randomNumber[i]]);
-        i++;
-      }
-
-      while (this.blueWords.length < 8) {
-        this.blueWords.push(this.selectedWords[randomNumber[i]]);
-        i++;
-      }
-
-      while (this.whiteWords.length < 7) {
-        this.whiteWords.push(this.selectedWords[randomNumber[i]]);
-        i++;
-      }
-      this.blackWord.push(this.selectedWords[randomNumber[i]]);
-      console.log(this.selectedWords);
-    },
-
-    // je compare le mot choisi au tableau rouge bleu et  blanc
-    setColor: function (word, index) {
-      let card = document.getElementsByClassName("card" + index);
-
-      if (this.redWords.find((Element) => Element == word) !== undefined) {
-        card[0].classList.add("isRed");
-      }
-      if (this.blueWords.find((Element) => Element == word) !== undefined) {
-        card[0].classList.add("isBlue");
-      }
-      if (this.whiteWords.find((Element) => Element == word) !== undefined) {
-        card[0].classList.add("isWhite");
-      }
-      if (this.blackWord.find((Element) => Element == word) !== undefined) {
-        card[0].classList.add("isBlack");
-      }
-    },
-    setColorForGrid: function (word) {
-      if (this.redWords.find((Element) => Element == word) !== undefined) {
-        return "isRedGrid";
-      }
-      if (this.blueWords.find((Element) => Element == word) !== undefined) {
-        return "isBlueGrid";
-      }
-      if (this.whiteWords.find((Element) => Element == word) !== undefined) {
-        return "isWhiteGrid";
-      }
-      if (this.blackWord.find((Element) => Element == word) !== undefined) {
-        return "isBlackGrid";
-      }
+    chat: function () {
+      this.displayChat = !this.displayChat;
     },
   },
   beforeMount() {
-    this.getRandomWord();
+    this.isConnected = true;
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.game {
+.game-zone {
+  margin-bottom: 150px;
+}
+
+.chatZone {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-  padding-bottom: 150px;
-
-  &-grid {
-    max-width: 1000px;
-    display: grid;
-    grid-template-columns: repeat(5, 20%);
-    grid-template-rows: repeat(5);
-  }
-  &-card {
-    background-image: url("../assets/cartes/carteBlanche.jpg");
-    background-size: 100% 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 70px;
-    margin: 2px;
-    max-width: 180px;
-    border: 1px solid rgb(206, 203, 191);
-    border-radius: 8px;
-    font-size: 15px;
-    &:hover {
-      cursor: pointer;
-      transform: scale(1.2);
-      transition: 0.2s;
-    }
-  }
-  &-word {
-    position: relative;
-    top: -7px;
-    &-number {
-      position: relative;
-      top: 10px;
-    }
-  }
-  &-chat {
-    margin-top: 40px;
-    width: 80%;
-  }
-  &-gessGrid {
-    display: flex;
-    flex-wrap: wrap;
-    width: 200px;
-    height: 200px;
-    border: solid 10px black;
-    border-inline-style: grey;
-    border-radius: 10px;
-    background: rgb(8, 8, 8);
-    &-square {
-      border-radius: 8px;
-      width: 20%;
-      height: 20%;
-      border: solid 1px black;
-    }
-  }
+  justify-content: center;
 }
-
-.isRed {
-  background-image: url("../assets/cartes/codeRouge.PNG");
-  z-index: 1;
-}
-.isBlue {
-  background-image: url("../assets/cartes/codeBleu.PNG");
-  z-index: 1;
-}
-.isBlack {
-  background-image: url("../assets/cartes/codeNoir2.PNG");
-  color: transparent;
-}
-.isWhite {
-  background-image: url("../assets/cartes/carteBlancheCivile2.png");
-  color: transparent;
-}
-.isRedGrid {
-  background-color: #ea5f22;
-}
-.isBlueGrid {
-  background-color: #10bed3;
-  object-fit: cover;
-}
-.isBlackGrid {
-  background-color: black;
-  object-fit: cover;
-}
-.isWhiteGrid {
-  background-color: #dfcfcc;
-  object-fit: cover;
-}
-
-@media all and (max-width: 1052px) {
-  .game-card {
-    &:hover {
-      transform: scale(1.1);
-    }
-  }
-}
-@media all and (max-width: 512px) {
-  .game-card {
-    font-size: 12px;
-  }
-} ;
 </style>
